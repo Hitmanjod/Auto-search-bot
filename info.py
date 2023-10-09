49dd13b9e18f32a56b7efe7438841c7534266d6a
Hitmanjod <142624499+Hitmanjod@users.noreply.github.com>
2023-10-09 09:19:40 +0530

    Update info.py

--- info.py
+++ info.py
@@ -21,7 +21,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "6630144705:AAEzzhvsrJYUi4ST8ITVn89_DGck46f
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
-PICS = (environ.get('PICS', 'https://graph.org/file/2f270952afa6bfc5a30db.mp4')).split() #SAMPLE PIC
+PICS = (environ.get('PICS', 'https://graph.org/file/44fa4c25b83f6a727413c.jpg')).split() #SAMPLE PIC
 NOR_IMG = environ.get("NOR_IMG", "https://te.legra.ph/file/a27dc8fe434e6b846b0f8.jpg")
 MELCOW_VID = environ.get("MELCOW_VID", "https://te.legra.ph/file/6f55d902f9bf2d0afd4bb.mp4")
 SPELL_IMG = environ.get("SPELL_IMG", "https://te.legra.ph/file/15c1ad448dfe472a5cbb8.jpg")
